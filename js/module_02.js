6c44dd89045ca05ea40df14b891483a4691e9744
Valentina <dert12345f@ukr.net>
2020-11-09 23:36:18 +0200

    module_02 task first

---
+++ js/module_02.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// Task first
+
+// const getItemsString = function(array) {
+//   'use strict';
+//   // Write code under this line
+// return 'Mango' + ' Poly' + ' Ajax' + ' Lux' + ' Jay' + ' Kong';
+//   const result = getItemsString(1, 2, 3, 4, 5, 6);
+// console.log(result);
+//   console.log(getItemsString(5, 10, 15));
+// };
+// console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
+
+
+// //for (const array of getItemsString) {  
+// //  console.log(array); } 
+
+// console.log(getItemsString.length);
+
+
+/*
+'1 - Mango
+2 - Poly
+3 - Ajax
+4 - Lux
+5 - Jay
+6 - Kong
+'
+*/
+
+//console.log(getItemsString([5, 10, 15]));
+/*
+'1 - 5
+2 - 10
+3 - 15
+'
+*/
