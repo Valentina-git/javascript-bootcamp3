f1c70290024eaf23151892904c1533932ee143c7
Valentina <dert12345f@ukr.net>
2020-12-04 17:32:52 +0200

    gallery__show-modal

--- js/practise.js/module_08.js/hw_8.js
+++ js/practise.js/module_08.js/hw_8.js
@@ -44,6 +44,7 @@
 Закрытие модального окна по нажатию клавиши ESC.
 Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
  */
+
 //========================== JS__SHOW-MODAL =========================================
 import galleryItems from './gallery-items.js';
 
@@ -60,41 +61,67 @@ let indexCurrentImage;
 refs.closeModalBtn.addEventListener('click', onCloseModal);
 refs.listGallery.addEventListener('click', onOpenModal);
 
-function createLi({original, preview, description}, index)  {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    const img = document.createElement('img');
+//=====================================================
 
-    li.classList.add('gallery__item');
-    a.classList.add('gallery__link');
-    img.classList.add('gallery__image');
-
-    a.href = original;
-    img.src = preview;
-    img.alt = description;
-    
-    img.setAttribute('data-source', original);
-    img.setAttribute('data-index', index);
-
-    a.append(img);
-    li.append(a);
-
-    return li;
-};
-
-function createGallery(galleryItems) {
-    
-    return galleryItems.map((liItem, index) => {
-
-        return createLi({
-            original: liItem.original,
-            preview: liItem.preview,
-            description: liItem.description
-        }, index)
-    })
+function createGallery() {
+    let markup = '';
+    for (let i = 0; i < galleryItems.length; i += 1) {
+        markup += `<li class="gallery__item">
+        <a
+          class="gallery__link"
+          href="${galleryItems[i].original}"
+        >
+          <img
+            class="gallery__image"
+            src="${galleryItems[i].preview}"
+            data-source="${galleryItems[i].original}"
+            alt="${galleryItems[i].description}"
+            data-index="${i}"
+          />
+        </a>
+      </li>`;
+    }
+    listGalleryRef.innerHTML = markup;
 }
+//=====================================================
 
-refs.listGallery.append(...createGallery(galleryItems));
+//=====================================================
+// function createLi({original, preview, description}, index)  {
+//     const li = document.createElement('li');
+//     const a = document.createElement('a');
+//     const img = document.createElement('img');
+
+//     li.classList.add('gallery__item');
+//     a.classList.add('gallery__link');
+//     img.classList.add('gallery__image');
+
+//     a.href = original;
+//     img.src = preview;
+//     img.alt = description;
+    
+//     img.setAttribute('data-source', original);
+//     img.setAttribute('data-index', index);
+
+//     a.append(img);
+//     li.append(a);
+
+//     return li;
+// };
+
+// function createGallery(galleryItems) {
+//     return galleryItems.map((liItem, index) => {
+//         return createLi({
+//             original: liItem.original,
+//             preview: liItem.preview,
+//             description: liItem.description
+//         }, index)
+//     })
+// }
+// refs.listGallery.append(...createGallery(galleryItems));
+//=====================================================
+
+
+
 
 
 function onOpenModal(event) {
@@ -117,6 +144,9 @@ function onOpenModal(event) {
 function onCloseModal() {
     refs.modal.classList.remove('is-open')
     refs.imageModal.src = ''
+
+    refs.closeModalBtn.removeEventListener('click', onCloseModal);
+    refs.overlay.removeEventListener('click', onCloseModal);
 };
 
 function onPressKey(event) { 
@@ -127,7 +157,7 @@ function onPressKey(event) {
         
         case 'ArrowRight':
             indexCurrentImage + 1 === galleryItems.length
-                ? indexCurrentImage = galleryItems.length - 1
+                ? indexCurrentImage = 0
                 : indexCurrentImage += 1;
             refs.imageModal.src = galleryItems[indexCurrentImage].original
             break;
@@ -143,6 +173,122 @@ function onPressKey(event) {
             break;
     }
 };
+//=====================================
+
+/**
+ * import galleryItems from './gallery-items.js';
+
+const listGalleryRef = document.querySelector('.js-gallery');
+const modalRef = document.querySelector('.js-lightbox');
+const imageModalRef = document.querySelector('.lightbox__image');
+const closeModalButton = document.querySelector(
+    'button[data-action="close-lightbox"]',
+);
+const overlayRef = document.querySelector('.lightbox__overlay');
+let indexCurrentImage;
+listGalleryRef.addEventListener('click', onOpenModal);
+
+// function createGallery() {
+//     let markup = '';
+//     for (let i = 0; i < galleryItems.length; i += 1) {
+//         markup += `<li class="gallery__item">
+//         <a
+//           class="gallery__link"
+//           href="${galleryItems[i].original}"
+//         >
+//           <img
+//             class="gallery__image"
+//             src="${galleryItems[i].preview}"
+//             data-source="${galleryItems[i].original}"
+//             alt="${galleryItems[i].description}"
+//             data-index="${i}"
+//           />
+//         </a>
+//       </li>`;
+//     }
+//     listGalleryRef.innerHTML = markup;
+// }
+function createLi({ original, preview, description }, index) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    const img = document.createElement('img');
+
+    li.classList.add('gallery__item');
+    a.classList.add('gallery__link');
+    img.classList.add('gallery__image');
+
+    a.href = original;
+    img.src = preview;
+    img.alt = description;
+    img.setAttribute('data-source', original);
+    img.setAttribute('data-index', index);
+    a.append(img);
+    li.append(a);
+    return li;
+}
+
+function createGallery(galleryItems) {
+    return galleryItems.map((liItem, index) => {
+        return createLi(
+            {
+                original: liItem.original,
+                preview: liItem.preview,
+                description: liItem.description,
+            },
+            index,
+        );
+    });
+}
+
+listGalleryRef.append(...createGallery(galleryItems));
+
+function onOpenModal(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    indexCurrentImage = Number(event.target.dataset.index);
+    modalRef.classList.add('is-open');
+    imageModalRef.src = event.target.dataset.source;
+    imageModalRef.alt = event.target.alt;
+    closeModalButton.addEventListener('click', onCloseModal);
+    overlayRef.addEventListener('click', onCloseModal);
+    window.addEventListener('keydown', onPressKey);
+}
+
+function onCloseModal() {
+    modalRef.classList.remove('is-open');
+    imageModalRef.src = '';
+}
+
+function onPressKey(event) {
+    switch (event.code) {
+        case 'Escape':
+            onCloseModal();
+            break;
+        case 'ArrowRight':
+            indexCurrentImage + 1 === galleryItems.length
+                ? (indexCurrentImage = 0)
+                : (indexCurrentImage += 1);
+            imageModalRef.src = galleryItems[indexCurrentImage].original;
+            break;
+
+        case 'ArrowLeft':
+            indexCurrentImage === 0
+                ? (indexCurrentImage = galleryItems.length - 1)
+                : (indexCurrentImage -= 1);
+            imageModalRef.src = galleryItems[indexCurrentImage].original;
+            break;
+
+        default:
+            break;
+    }
+}
+
+// createGallery();
+ */
+
+//=====================================
 
 //========================== JS-GALLERY =========================================
 
